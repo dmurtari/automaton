61eb62f417a9494a30d3a6aae0181c62ecb6cfce
dmurtari <domenic.murtari@gmail.com>
2013-10-27 23:01:22 -0600

    Catching AttributeError on bad key

--- automaton.py
+++ automaton.py
@@ -45,13 +45,11 @@ class Automaton(Thread):
         for cluster in self.clusters.list:
             if self.config.options.launch_cluster:
                 cluster.connect()
-                
+                # Catches AttributeError
                 try:
                     cluster.launch()
-                except Exception as ex:
-                    template = "An exception of type {0} occured. Arguments:\n{1!r}"
-                    message = template.format(type(ex).__name__, ex.args)
-                    print message
+                except AttributeError as e:
+                    print "Caught AttributeError Exception"
                 
             if self.config.options.terminate_cluster:
                 cluster.connect()
