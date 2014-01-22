790dc2e9370c01ab7287a92c2199175accf88f6f
dmurtari <domenic.murtari@gmail.com>
2014-01-21 20:58:23 -0700

    Working with openstack connection through boto

--- openstack_conn.py
+++ openstack_conn.py
@@ -6,10 +6,11 @@ access_id = os.environ['AWS_ACCESS_KEY']
 secret_key = os.environ['AWS_SECRET_KEY']
 cloud_uri = '198.202.120.83'
 cloud_port = '8773'
+path = '/services/Cloud'
 image_id = 'futuregrid/ubuntu-12.04'
 cloud_type = 'grizzly'
 
 region = RegionInfo(name=cloud_type, endpoint=cloud_uri)
-conn = EC2Connection(access_id, secret_key, is_secure=False, 
-                     path='/services/Cloud/', port = cloud_port, 
-                     region=region, validate_certs=False)
+conn = EC2Connection(access_id, secret_key, is_secure=False, path=path, 
+                     port = cloud_port, region=region, validate_certs=False)
+conn.get_all_images()
