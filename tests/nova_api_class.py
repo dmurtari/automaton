721ae47ce6b6a72741da68bc7ae86091014486e0
Domenic Murtari <domenic.murtari@gmail.com>
2014-01-29 12:14:07 -0700

    Nova client class can delete instances

---
+++ tests/nova_api_class.py
@@ -0,0 +1,50 @@
+import os
+import time
+import novaclient.exceptions as exception
+import novaclient.v1_1.client as nvclient
+from credentials import get_nova_creds
+
+class NovaClient(object):
+
+    def __init__(self):
+        self.creds = get_nova_creds()
+        self.nova = nvclient.Client(**self.creds)
+
+        print "Checking for keypair and importing if not found"
+        if not self.nova.keypairs.findall(name="mykey"):
+            with open(os.path.expanduser('~/.ssh/id_rsa.pub')) as fpubkey:
+                self.nova.keypairs.create(name="mykey", public_key=fpubkey.read())
+
+
+        self.image = self.nova.images.find(name="futuregrid/ubuntu-12.04")
+        self.flavor = self.nova.flavors.find(name="m1.tiny")
+
+    def create(self, name):
+        print "Creating instance of " + str(self.image) + " of flavor " + str(self.flavor)
+        instance = self.nova.servers.create(name=name, image=self.image, flavor=self.flavor, key_name="mykey")
+
+        # Poll at 5 second intervals, until the status is no longer 'BUILD'
+        status = instance.status
+        while status == 'BUILD':
+            time.sleep(5)
+            # Retrieve the instance again so the status field updates
+            instance = self.nova.servers.get(instance.id)
+            status = instance.status
+        print "status: %s" % status
+
+    def destroy(self, name):
+        print "Deleting instance " + name
+        try:
+            server = self.nova.servers.find(name=name)
+            server.delete()
+        except exception.NotFound:
+            print "Instance of name " + name + " not found"
+
+def main():
+    conn = NovaClient()
+    #conn.create("test")
+    conn.destroy("test")
+
+if __name__ == "__main__":
+    main()
+
